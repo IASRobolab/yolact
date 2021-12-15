de4e55d87a000e697306f77e2041871db6e520e3
Federico Rollo <rollo.f96@gmail.com>
2021-12-15 16:18:44 +0100

    added setup.py to create the whl to install the yolact library

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='yolact',
+      version='1.0.0',
+      description='Pytorch Library of yolact for Instance Segmentation',
+      author='Federico :)',
+      author_email='robolab.leonardo@gmail.com',
+      url='https://github.com/robolableonardo/yolact',
+      install_requires=[
+          'numpy', 'torch==1.4.0', 'torchvision==0.5.0', 'Pillow', 'scipy',
+          'scikit-learn', 'metric-learn', 'cython', 'opencv-python',
+          'pycocotools', 'matplotlib'],
+      packages=find_packages(),
+      keywords=[
+          'Instance Segmentation',
+          'Deep Learning',
+      ])

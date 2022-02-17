def5a79000cb6d3588385f587cd90f2b7dd594f9
Andrea Zunino <andreazuna@hotmail.it>
2022-02-17 11:36:51 +0100

    modified setup.py metadata

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(name='yolact',
       version='1.0.0',
       description='Pytorch Library of yolact for Instance Segmentation',
-      author='Federico :)',
-      author_email='robolab.leonardo@gmail.com',
+      author='Federico Rollo',
+      author_email='rollo.f96@gmail.com',
       url='https://github.com/robolableonardo/yolact',
       install_requires=[
           'numpy', 'torch==1.4.0', 'torchvision==0.5.0', 'Pillow', 'scipy',
